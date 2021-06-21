5c928704ffc07602bf41a0a936342570c497253b
Opata1234 <85105036+Opata1234@users.noreply.github.com>
2021-06-21 16:41:10 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+	  
+	var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+	
+	var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    var birthday=document.getElementById('datepicker').value;
+    var male=document.getElementById('male');
+    var female=document.getElementById('female');
+
+    var date=new Date(birthday);
+    var weekday=date.getDay();
+
+    function result(){
+        if(male.checked===true){
+            alert('You were born on ' + daysOfWeek[weekday] + 'and your akan name is ' + maleNames[weekday])
+        }
+        if(female.checked===true){
+            alert('You were born on ' + daysOfWeek[weekday] + 'and your akan name is ' + femaleNames[weekday])
+        }
+    }
